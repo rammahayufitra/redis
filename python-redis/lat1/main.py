e8c8f9e8932d412084613140ffa2638e91ffcca3
ramma.fitra <rammahayufitra@gmail.com>
2023-03-05 00:26:13 +0700

    [dev] redis dan data json

--- python-redis/lat1/main.py
+++ python-redis/lat1/main.py
@@ -1,6 +1,8 @@
 # redis dan json 
 import json 
 import redis 
+r = redis.Redis(decode_responses=True)
+print(r.ping())
 
 data = {
     'nama':'ramma', 
@@ -10,3 +12,9 @@ data = {
     },
     'bahasa program': ['python','C++','javascript']
 }
+
+set_key1 = r.set("data", json.dumps(data))
+print(set_key1)
+get_key1 = r.get("data")
+json_load = json.loads(get_key1)
+print(json_load)
