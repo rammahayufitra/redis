68d4092784d26a7d18b99f9a5b89c2df20a7828e
ramma.fitra <rammahayufitra@gmail.com>
2023-03-05 00:20:22 +0700

    [dev] redis dan json

--- python-redis/lat1/main.py
+++ python-redis/lat1/main.py
@@ -1,13 +1,12 @@
+# redis dan json 
+import json 
 import redis 
-r = redis.Redis(decode_responses=True)
-print(r.ping())
-#melakukan set key dan get key 
-r.set("ramma1", "hallo ramma")
-print(r.get("ramma1"))
-#melakukan multiple set dan get
+
 data = {
-    "ramma1": "hallo ramma1",
-    "ramma2": "hallo ramma2"
+    'nama':'ramma', 
+    'usia': 30,
+    'alamat': {
+        'kota': 'jakarta barat'
+    },
+    'bahasa program': ['python','C++','javascript']
 }
-r.mset(data)
-print("1.", r.get("ramma1"), "2.", r.get("ramma2"), "3.", r.mget("ramma1", "ramma2"))
